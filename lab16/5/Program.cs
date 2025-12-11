66b8405c72edcf9cece72a8d75675078cba77293
CATX <golubeva.ludmila12345@gmail.com>
2025-12-11 14:38:48 +0300

    Add files via upload

---
+++ lab16/5/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main()
+    {
+        var t1 = new Time(14, 35, 9);
+        var t2 = new Time(9, 10, 5);
+
+        Console.WriteLine(t1 > t2);
+        Console.WriteLine(t1[1]);
+
+        t1[2] = 59;
+        Console.WriteLine(t1);
+    }
+}
